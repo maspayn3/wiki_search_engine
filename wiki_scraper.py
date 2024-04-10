01e0303364b9df320b7dab097e189084f2ae3fc9
maspayn3 <maspayne99@gmail.com>
2024-04-10 13:59:34 -0400

    Working wiki web scraper. Data cleaning may be used in map-reduce pipeline instead

---
+++ wiki_scraper.py
@@ -0,0 +1,146 @@
+import argparse
+import requests
+import re
+import time
+import csv
+import random
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser()
+
+# -r -c
+parser.add_argument("-r", '--randomize_search', action='store_true')
+parser.add_argument('-c', '--clean_text', action='store_true')
+parser.add_argument('-s', '--search', action='store')
+
+args = parser.parse_args()
+
+def get_wiki_text(soup: BeautifulSoup):
+    """Extracts body of text from a wikipedia article."""
+    body = soup.find(id='bodyContent')
+    unclean_text = body.find_all(['p', 'h2'])
+
+    final_text = []
+
+    pattern = r'\[[^\]]*\]'
+    compiled_pattern = re.compile(pattern)
+    for text in unclean_text:
+        # clean text of [] and \n
+        cleaned_text = compiled_pattern.sub('', text.get_text()).rstrip()
+        cleaned_text = cleaned_text.replace("\n", '')
+        final_text.append(cleaned_text)
+        
+    return ''.join(final_text)
+
+
+def get_wiki_title(soup: BeautifulSoup):
+    title = soup.find(id="firstHeading").get_text()
+    return title
+
+
+def get_links_from_article(soup: BeautifulSoup):
+    """Collects all links to other wikipedia articles from a single wiki article."""
+    body = soup.find(id="bodyContent")
+    links = body.find_all('a', href=True)
+
+    wiki_links_list = []
+    wiki_links_set = set()
+
+    # need to clean the links for only other wiki articles
+    for link in links:
+        # link here is a Tag class object and [] is the access operator
+        if not link['href'].startswith("/wiki/"):
+            continue
+
+        # Considering skipping files, but some contain useful things
+        temp = link['href'].split('/')[2]
+
+        if temp.startswith("File:", 0, 5):
+            continue
+
+        elif temp.startswith("Category:", 0, 8):
+            continue
+        
+        elif temp.startswith("User:", 0, 5):
+            continue
+        
+        elif temp.startswith("Special:BookSources"):
+            continue
+
+        wiki_links_list.append(link['href'])
+    wiki_links_set.update(wiki_links_list)
+
+    return wiki_links_set
+
+
+def get_article(article_link):
+    response = requests.get(url=f"https://en.wikipedia.org/{article_link}")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
+    
+    else:
+        print(f"Status code: {response.status_code}")
+        exit(1)
+
+
+def download_wiki_text(randomize_list: bool, clean_text_flag: bool, starting_wiki: bool):
+    """Scapes initial wiki article for text and links to branch and build data.csv."""
+    # planned to keep track of origin article and all 
+    # linked articles to create a graph
+    to_be_scraped = set([starting_wiki])
+    
+    with open('wiki_num_token.txt', mode='r') as file:
+        token = int(file.readline())
+
+    try:
+        while to_be_scraped:
+            if randomize_list is True:
+                soup = get_article(random.choice(list(to_be_scraped)))
+            else:
+                soup = get_article(to_be_scraped.pop())
+
+            title = get_wiki_title(soup)
+            text = get_wiki_text(soup)
+
+            # clean text for usage in inverted index
+            if clean_text_flag is True:
+                text = clean_text(text)
+
+            to_be_scraped.update(get_links_from_article(soup))
+            wiki_data_entry = [(token, title, text)]
+
+            print(f"Visting... {title}")
+            with open("data.csv", mode='a', newline='\n', encoding='utf-8') as file:
+                writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+                writer.writerows(wiki_data_entry)
+
+                token += 1
+
+            time.sleep(5)
+
+    except KeyboardInterrupt:
+        print("Ending search")
+
+        with open("wiki_num_token.txt", mode='w') as file:
+            file.write(str(token))
+
+
+def clean_text(text):
+    """Prepares text for usage in an inverted index."""
+    # removes non alphanumerics, case sensitivity, and stop words
+    text = re.sub (r"[^a-zA-Z0-9 ]+", "", text)
+    text = text.casefold()
+
+    return text
+
+
+if __name__ == "__main__":
+    print("Press ctrl + c to end search")
+
+    start_time = time.time()
+    print(args.randomize_search, args.clean_text, args.search)
+    download_wiki_text(randomize_list=args.randomize_search, clean_text_flag=args.clean_text, starting_wiki=args.search)
+    end_time = time.time()
+
+    print("Elapsed time:", end_time - start_time, "seconds")
