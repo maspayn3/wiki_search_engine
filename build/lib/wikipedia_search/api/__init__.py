e9b22e8d77cecc77aeb892a94e18ecf880b93b67
maspayn3 <maspayne99@gmail.com>
2024-11-09 02:31:23 -0500

    Project structure changes

--- build/lib/wikipedia_search/api/__init__.py
+++
@@ -1,2 +0,0 @@
-"""API package initialization. Empty as routes are defined in routes.py."""
-# This can be empty - it just marks the directory as a Python package
--- build/lib/wikipedia_search/search/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Search package initialization. Manages the search index instance."""
-from wikipedia_search.search.index_loader import SearchIndex, init_app
-
-# Create global search index instance
-search_index = SearchIndex()
-
-# Make these available when importing from search package
-__all__ = ['search_index', 'init_app']
